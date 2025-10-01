b3253e4737c8865ac58e6501b67ded01d034b47e
Frank-ke254 <kahotadfrajeh@gmail.com>
2025-10-01 13:15:58 +0300

    Updated files

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import permissions
 from drf_yasg.views import get_schema_view
-from drf_yasg import openapi 
+from drf_yasg import openapi
+from rest_framework import permissions
 
 schema_view = get_schema_view(
-    openapi.Info(
-        title="ALX Travel App API",
-        default_version="v1",
-        description="ALX Travel App API documentation.",
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
+   openapi.Info(
+      title="Travel App API",
+      default_version='v1',
+      description="API for managing listings and bookings",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('listings/', include('listings.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout = 0), name='schema-swagger-ui')
+    path('api/', include('listings.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- alx_travel_app/listings/urls.py
+++ alx_travel_app/listings/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ListingViewSet, BookingViewSet
 from . import views
+
+
+router = DefaultRouter()
+router.register(r'listings', ListingViewSet)
+router.register(r'bookings', BookingViewSet)
+
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('', include(router.urls)),
 ]
